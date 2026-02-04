4479a971f0228fdba3b8a4259a5fccc033b2b487
proyrbreginald <proyrbreginald@gmail.com>
2026-02-04 20:39:01 +0800

    修改链接脚本位置索引；修改配置结构体的成员命名

--- include/loader.h
+++ include/loader.h
@@ -26,11 +26,11 @@ typedef struct
 /* 配置信息 */
 typedef struct
 {
-    volatile uint8_t boot_mode;    // 启动模式：0=正常启动，1=强制app_a，2=强制app_b
-    volatile loader_info_t loader; // 引导程序信息
-    volatile app_info_t app_a;     // app_a信息
-    volatile app_info_t app_b;     // app_b信息
-    volatile uint8_t invalid[22];  // 32字节对齐
+    volatile uint8_t boot_mode;     // 启动模式：0=正常启动，1=强制app_a，2=强制app_b
+    volatile loader_info_t loader;  // 引导程序信息
+    volatile app_info_t user;       // app_a信息
+    volatile app_info_t oem;        // app_b信息
+    volatile uint8_t invalid[22];   // 32字节对齐
 } config_t;
 _Static_assert(0 == (sizeof(config_t) & 31), "size must be a multiple of 32 bytes"); // 静态检查：32字节对齐
 
